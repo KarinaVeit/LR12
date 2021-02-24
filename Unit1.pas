07801a6fc24f07f5f95f5f33c46c2fe2ee20f09e
KarinaVeit <smolkova.karina@yandex.ru>
2021-02-24 10:05:54 +0700

    first commit

---
+++ Unit1.pas
@@ -0,0 +1,90 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+TParallel = class;
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Memo1: TMemo;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+TParallel = class
+  public
+  Fa:word; //поле хранящее ширину
+  Fb:word; //поле хранящее длину
+  Fc:word; //поле хранящее высоту
+  procedure Init (a,b,c: word);
+  function Volume: word; //метод-функция
+                         //возвращающая объем
+  procedure Show;
+
+    end; //конец описания класса
+
+var
+  Form1: TForm1;   Par1:TParallel;    Par:array [1..5] of TParallel;
+  x:word=0;
+  y:word=0;
+  z:word=0;
+
+implementation
+
+{$R *.dfm}
+
+{ TParallel }
+procedure TParallel.Init(a, b, c: word);
+begin
+  Fa:=a;
+  Fb:=b;
+  Fc:=c;
+end;
+
+procedure TParallel.Show;
+begin
+  ShowMessage('Объем параллелепипеда равен'
+  + IntToStr(Volume)+#10#13 + 'Ширина-Поле Fa='+IntToStr(Fa)+#10#13 + 'Длина-Поле Fb='+IntToStr(Fb)+#10#13 + 'Высота-Поле Fc='+IntToStr(Fc)+#10#13);
+end;
+
+function TParallel.Volume: word;
+begin
+ result:=Fa*Fb*Fc; {возвращаем во внешнюю программу объемы в виде значения типа word}
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  x:=x+1; y:=y+1; z:=z+1;
+  Par1:=TParallel.Create;
+  Par1.Init(x,y,z);
+  Par1.Show;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  if Par1 = nil then
+  Form1.Caption:='Объекта в памяти нет'
+    else
+    Form1.Caption:='Объект существует в памяти';
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+    i:integer;
+begin
+  for  i := 1 to 5 do
+    Memo1.Lines.Add(('Адрес объекта с номером' +IntToStr(i)+ 'равен' +IntToStr (integer(Par[i])) + 'Поле Fa=' +IntToStr(Par[i].Fa)));
+end;
+
+end.
